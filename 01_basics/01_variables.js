f015c13ce37c77ead20fad85b97d2ad0e0789eb3
AKHER UDDIN AHMED <117501410+akheruddinahmed@users.noreply.github.com>
2025-02-04 04:42:10 +0000

    var,let,const done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123
+let accountEmail="akheruddinahmedcse@gmail.com"
+var accountPassword="123"
+accountCity="Guwahati"
+let accountState
+
+// accountId = 12345 //not allowed
+console.log(accountId);
+accountPassword = "1234"
+accountCity="Bongaigaon"
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
