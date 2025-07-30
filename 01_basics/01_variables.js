dd2b16601fd561ec807f13b4115e226959012ee5
AKHER UDDIN AHMED <117501410+akheruddinahmed@users.noreply.github.com>
2025-07-30 01:12:13 +0000

    Comment Added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -9,4 +9,9 @@ console.log(accountId);
 accountPassword = "1234"
 accountCity="Bongaigaon"
 
+/*
+prefer not to use var
+bacause of issue in block scope and functional scope
+*/
+
 console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
